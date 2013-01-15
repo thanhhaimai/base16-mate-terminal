91ab440497d46c728b10c045e1ca07eb32c729da
Thanh Hai Mai <thanhhaipmai@gmail.com>
2013-01-14 18:59:01 -0800

    - fixed Chalk

--- base16-chalk.dark.sh
+++ base16-chalk.dark.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# Base16 Chalk - Gnome Terminal color scheme install script
-# Chris Kempson (http://chriskempson.com)
+# Base16 Chalk - Mate Terminal color scheme install script
+# Thanh Hai Mai (https://github.com/thanhhaimai)
 
 [[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="Base 16 Chalk"
 [[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG="base-16-chalk"
-[[ -z "$GCONFTOOL" ]] && GCONFTOOL=gconftool
-[[ -z "$BASE_KEY" ]] && BASE_KEY=/apps/gnome-terminal/profiles
+[[ -z "$GCONFTOOL" ]] && GCONFTOOL="mateconftool-2"
+[[ -z "$BASE_KEY" ]] && BASE_KEY=/apps/mate-terminal/profiles
 
 PROFILE_KEY="$BASE_KEY/$PROFILE_SLUG"
 
@@ -34,7 +34,7 @@ glist_append() {
 }
 
 # Append the Base16 profile to the profile list
-glist_append string /apps/gnome-terminal/global/profile_list "$PROFILE_SLUG"
+glist_append string /apps/mate-terminal/global/profile_list "$PROFILE_SLUG"
 
 gset string visible_name "$PROFILE_NAME"
 gset string palette "#115511551155:#220022002200:#330033003300:#550055005500:#bb00bb00bb00:#dd00dd00dd00:#ee00ee00ee00:#ff55ff55ff55:#ffbb99ffbb11:#eeddaa998877:#ddddbb2266ff:#aacccc226677:#1122ccffcc00:#66ffcc22eeff:#ee11aa33eeee:#ddeeaaff88ff"
@@ -44,3 +44,4 @@ gset string bold_color "#dd00dd00dd00"
 gset bool   bold_color_same_as_fg "true"
 gset bool   use_theme_colors "false"
 gset bool   use_theme_background "false"
+
